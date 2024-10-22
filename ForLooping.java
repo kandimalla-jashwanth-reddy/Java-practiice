593d129aecd93a206396de8d60272f38faa97318
kandimalla jashwanth reddy <119109241+kandimalla-jashwanth-reddy@users.noreply.github.com>
2024-10-22 19:55:06 +0530

    Add files via upload

---
+++ ForLooping.java
@@ -0,0 +1,124 @@
+//
+///*print 1 to 100 numbers */
+//
+//
+////package Reddy.Assessments;
+////
+////public class ForLooping {
+////
+////	public static void main(String[] args) {
+////		// TODO Auto-generated method stub
+////
+////		int i;
+////		for(i=1;i<=100;i++) {
+////			System.out.println(i);
+////		}}
+////}
+//
+//
+//
+//
+//
+///*print the even numbers between 200 to 500*/
+////package Reddy.Assessments;
+////
+////public class ForLooping {
+////
+////	public static void main(String[] args) {
+////		int j;
+////		for(j=200;j<=500;j++) {
+////			if(j%2==0)
+////				System.out.println(" The even number is:"+j);	
+////		}
+////		}
+////	}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+///* divisible by 7  from the 150 to 200*/
+////package Reddy.Assessments;
+////
+////public class ForLooping {
+////
+////	public static void main(String[] args) {
+////		int i;
+////		for(i=150;i<=200;i++) {
+////			if(i%7==0)
+////			System.out.println("Divisible by 7 from 150 to 200 is:"+i);
+////		}
+////	}
+////	}
+//
+//
+//
+//
+//
+//
+//
+/////*sum of number between 40 to 80*/
+////package Reddy.Assessments;
+////
+////public class ForLooping {
+////	public static void main(String[] args) {
+////		int n=100;
+////		int i;
+////		int sum =0;
+////		for(i=40;i<=80;i++) {
+////			sum +=i;
+////		}
+////		System.out.println(sum);
+//////		
+//////		int sum=(n*(n+1)/2);
+//////		System.out.println(sum);
+////	}
+////}
+//
+//
+//
+//
+//
+////package Reddy.Assessments;
+////
+////public class ForLooping {
+////	public static void main(String[] args) {
+////		
+////		int i;
+////		for(i=200;i>=25;i--) {
+////			if(i%2!=0) {
+////			System.out.println("Print the odd numbers: "+i);
+////		}
+////		
+////	}
+////	}
+////}
+//
+//package Reddy.Assessments;
+//
+//public class ForLooping {
+//    public static void main(String[] args) {
+//        
+//        int i;
+//        // Looping from 200 down to 25
+//        for(i = 200; i >= 25; i--) {
+//            // Check if the number is odd
+//            if(i % 2 != 0) {
+//                // Print the odd numbers
+//                System.out.println("Print the odd number: " + i);
+//            }
+//        }
+//    }
+//}
+
+
+
+
+
