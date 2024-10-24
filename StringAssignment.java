79b3857254fe815badabf909076186efa7b6dc89
kandimalla jashwanth reddy <119109241+kandimalla-jashwanth-reddy@users.noreply.github.com>
2024-10-24 19:40:13 +0530

    Add files via upload

---
+++ StringAssignment.java
@@ -0,0 +1,18 @@
+
+//1.question
+package Reddy.Assessments;
+import java.util.Scanner;
+public class StringAssignment {
+
+	public static void main(String[] args) {
+		Scanner  scanner =new Scanner(System.in);
+		System.out.println("Enter the word:");
+		String  a1=scanner.nextLine();
+		System.out.println("The lengthof the text is:"+a1.length());
+	}
+
+}
+
+
+
+
