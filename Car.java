83e2c6b98141aab78aad10e102d986a83007caa0
kandimalla jashwanth reddy <119109241+kandimalla-jashwanth-reddy@users.noreply.github.com>
2024-10-25 19:46:38 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,13 @@
+package AccessModifiers;
+
+public class Car {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s= new Student();
+		System.out.println(s.rollNo);
+		s.printRollNumber();
+	 
+	}
+
+}
