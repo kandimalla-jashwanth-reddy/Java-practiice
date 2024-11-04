44b2f00f2529c8193c67e67da0d102d2699bad56
kandimalla jashwanth reddy <119109241+kandimalla-jashwanth-reddy@users.noreply.github.com>
2024-11-04 10:30:31 +0530

    Add files via upload

---
+++ SampleAC.java
@@ -0,0 +1,14 @@
+package Jashwanth.Interface;
+
+public abstract class SampleAC implements Laptop {
+
+	public void copy() {
+		System.out.println("Lenovo copy code");
+	}
+	public void paste() {
+		System.out.println("Lenovo paste code");
+		
+	}
+
+
+}
